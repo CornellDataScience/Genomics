441a073fda041c3057f4312cfd2bb81ebf8cee42
Cameron Ibrahim <cai29@cornell.edu>
2018-05-04 17:32:06 -0400

    Readded Val set, fixed issue with collate function that was forming batches wrong, revealed new issue with graph construction

--- enc.py
+++ enc.py
@@ -16,23 +16,23 @@ dataset = mgd.MaizeGeneDataset('assets/X.csv',
                                targets,
                                mode="melt")
 
-# valset = mgd.MaizeGeneDataset('assets/X.csv',
-#                               'assets/y.csv',
-#                               'val',
-#                               inputs,
-#                               targets,
-#                               mode="melt")
+valset = mgd.MaizeGeneDataset('assets/X.csv',
+                              'assets/y.csv',
+                              'val',
+                              inputs,
+                              targets,
+                              mode="melt")
 
 
 basemap = {'A': 0, 'T': 1, 'C': 2, 'G': 3}
 
 
 def mycollate(batch):
-    b = torch.zeros(len(batch), 4, len(batch[0]), requires_grad=True)
-    m1 = torch.zeros(len(batch), 4, len(batch[0]), dtype=torch.float)
-    m2 = torch.zeros(len(batch), 4, len(batch[0]), dtype=torch.uint8)
+    b = torch.zeros(len(batch), 4, len(batch[0]["sequence"]), requires_grad=True)
+    m1 = torch.zeros(len(batch), 4, len(batch[0]["sequence"]), dtype=torch.float)
+    m2 = torch.zeros(len(batch), 4, len(batch[0]["sequence"]), dtype=torch.uint8)
     for i, gene in enumerate(batch):
-        for j, c in enumerate(gene):
+        for j, c in enumerate(gene['sequence']):
             if c in basemap:
                 b[i, basemap[c], j] = 1
                 m1[i, :, j] = 1
@@ -41,25 +41,14 @@ def mycollate(batch):
 
 
 print("start load")
-dataloader = DataLoader(dataset, batch_size=32, num_workers=8, collate_fn=mycollate)
-# valloader = DataLoader(valset, batch_size=32, num_workers=8)
+dataloader = DataLoader(dataset, batch_size=32, num_workers=0, collate_fn=mycollate)
 del dataset
 print("end load")
+print("start load")
+valloader = DataLoader(valset, batch_size=32, num_workers=0, collate_fn=mycollate)
+del valset
+print("end load")
 
-# val = []
-# for batch in valloader:
-#     batch = batch['sequence']
-#     b = torch.zeros(len(batch), 4, len(batch[0]), requires_grad=True)
-#     m = torch.zeros(len(batch), 1, len(batch[0]), requires_grad=True)
-#     for i, gene in enumerate(batch):
-#         for j, c in enumerate(gene):
-#             if c in basemap:
-#                 b[i, basemap[c], j] += 1
-#                 b[i, 1,  j] += 1
-#     val.append((b, m))
-#
-# del valset
-# del valloader
 
 net = Noisey(4, [1, 12, 9, 6, 3], 64, 0.2, 0.1)
 net = net.cuda()
@@ -77,6 +66,7 @@ for epoch in range(epochs):
     for i, (genes, m1, m2) in enumerate(dataloader):
         genes = genes.cuda()
         m1 = m1.cuda()
+        m2 = m2.cuda()
         outputs = net(genes, m1)
 
         optimizer.zero_grad()
@@ -87,19 +77,20 @@ for epoch in range(epochs):
 
         print("Train Loss: {}\n".format(loss.data))
         logfile.write("Epoch {} Batch {} Train Loss: {}\n".format(epoch, i, loss.data))
-    # if epoch > 0 and epoch % 5 == 0:
-    #     tmperror = 0
-    #     numits = 0
-    #     for batch in val:
-    #         batch = batch.cuda()
-    #         outputs = net(batch)
-    #         loss = loss_func(outputs.data, batch.data)
-    #         tmperror += loss
-    #         numits += 1
-    #     val_error.append(tmperror / numits)
-    #     if len(val_error) > 0 and val_error[-1] > val_error[-2]:
-    #         break
-    #     logfile.write("\nValidation Loss: {}\n\n".format(loss.data))
+    if epoch > 0 and epoch % 5 == 0:
+        tmperror = 0
+        numits = 0
+        for i, (genes, m1, m2) in enumerate(valloader):
+            genes = genes.cuda()
+            m1 = m1.cuda()
+            m2 = m2.cuda()
+            loss = loss_func(genes[m2], outputs.data[m2])
+            tmperror += loss
+            numits += 1
+        val_error.append(tmperror / numits)
+        if len(val_error) > 0 and val_error[-1] > val_error[-2]:
+            break
+        logfile.write("\nValidation Loss: {}\n\n".format(loss.data))
     torch.save(net, "checkpoint.pt")
 
 
